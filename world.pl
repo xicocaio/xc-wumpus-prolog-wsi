4745eb0e842cea8e24ba93032944ddc691622d33
Francisco Paiva <xicocaio@gmail.com>
2018-05-03 15:01:59 -0300

    Add successor state axioms for moving

--- world.pl
+++ world.pl
@@ -27,6 +27,22 @@ left(X,Y, X_Next,Y_Next) :- X_Next is X - 1, Y_Next is Y.
 
 breeze(P, Q) :- pit(X,Y), (up(X,Y,P,Q); down(X,Y,P,Q); right(X,Y,P,Q); left(X,Y,P,Q)).
 
+forward(X) :- X.
+
+%%% (X,Y,T), start location is at position (1,1) at time = 1
+start_location(1,1,1).
+looking_right(D) :- D = 'right'.
+
+location(X_Next,Y_Next,T_Next) :- start_location(X,Y,T), T_Next is T + 1, X_Next is X + 1, Y_Next is Y, looking_right('right'), forward(true).
+
+
+%%% findall(Y, pit(3,Y), List).
+
+%%% axioms page 284
+
+%%% page 359 teaches about lists
+
+
 % Test world
 %     +---+---+---+---+
 %   4 |   |   |   | G |
